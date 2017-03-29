bdb568cc71b0fb17f3dd595dc8c3e118bf18d8b8
Devin Rieger <dev0089@gmail.com>
2017-03-29 12:02:54 -0400

    added 4x comparisons, limited roster size, removed get from mouseover

--- index.js
+++ index.js
@@ -8,14 +8,17 @@ Array.from({length:numTypes},(v,k)=>k+1).map(function(number) {
 })
 
 class Pokemon {
-  constructor(number, name, types, sprite, team, weak_to, not_weak_to) {
+  constructor(number, name, types, stats, sprite, team, weak_to, not_weak_to, super_effective, not_very_effective) {
     this.number = number,
     this.name = name,
     this.types = types,
-    this.sprite = sprite
-    this.team = team
-    this.weak_to = weak_to
-    this.not_weak_to = not_weak_to
+    this.stats = stats,
+    this.sprite = sprite,
+    this.team = team,
+    this.weak_to = weak_to,
+    this.not_weak_to = not_weak_to,
+    this.super_effective = super_effective,
+    this.not_very_effective = not_very_effective
   }
   static intialize(numOrName, team) {
     return $.get(`https://pokeapi.co/api/v2/pokemon/${numOrName}`).
@@ -26,11 +29,13 @@ class Pokemon {
           return type.name == ptype.type.name
         })[0]
       })
-      var pokemon = new Pokemon(pokemon.id, pokemon.name, types, sprite, team, [], [])
+      var pokemon = new Pokemon(pokemon.id, pokemon.name, types, pokemon.stats, sprite, team, [], [], [], [])
       if (pokemon.team == 1) {
         your_roster.push(pokemon)
+        allPokes.push(pokemon)
       } else {
         enemy_roster.push(pokemon)
+        allPokes.push(pokemon)
       }
       for (var j = 0; j < pokemon.types.length; j++) {
         var type = pokemon.types[j]
@@ -40,6 +45,18 @@ class Pokemon {
         for (var k = 0; k < type.damage_relations.half_damage_from.length; k++) {
           pokemon.not_weak_to.push(type.damage_relations.half_damage_from[k].name)
         }
+        for (var k = 0; k < type.damage_relations.no_damage_from.length; k++) {
+          pokemon.not_weak_to.push(type.damage_relations.no_damage_from[k].name)
+        }
+        for (var k = 0; k < type.damage_relations.double_damage_to.length; k++) {
+          pokemon.super_effective.push(type.damage_relations.double_damage_to[k].name)
+        }
+        for (var k = 0; k < type.damage_relations.half_damage_to.length; k++) {
+          pokemon.not_very_effective.push(type.damage_relations.half_damage_to[k].name)
+        }
+        for (var k = 0; k < type.damage_relations.no_damage_to.length; k++) {
+          pokemon.not_very_effective.push(type.damage_relations.no_damage_to[k].name)
+        }
       }
       return pokemon
     })
@@ -48,6 +65,7 @@ class Pokemon {
 
 var your_roster = []
 var enemy_roster = []
+var allPokes = []
 
 $(function() {
   $(".pokeSubmit").on("click", function() {
@@ -58,16 +76,22 @@ $(function() {
     } else {
       var team = 2
     }
-    Pokemon.intialize(input, team).
-    then(function(pokemon){
-      if (pokemon.team == 1){
-        var div = 'pokemon'
-      } else {
-        var div = 'matchup'
-      }
-      let a_tag = `<a href='#' data-pokemon-name='${pokemon.name}' onmouseover='pokemonInfo(this, "${pokemon.name}")' onclick="removePokemon(this)">`
-      $('.container').find(`.${div}:empty:first`).html(a_tag + "<img src=" + pokemon.sprite + "></a>")
-    })
+    if (team == 1 && your_roster.length >= 6) {
+      alert("Your roster is maxed out!")
+    } else if (team == 2 && enemy_roster.length >= 6) {
+      alert("Your roster is maxed out!")
+    } else {
+      Pokemon.intialize(input, team).
+      then(function(pokemon){
+        if (pokemon.team == 1){
+          var div = 'pokemon'
+        } else {
+          var div = 'matchup'
+        }
+        let a_tag = `<a href='#' data-pokemon-name='${pokemon.name}' onmouseover='pokemonInfo(this, "${pokemon.name}")' onclick="removePokemon(this)">`
+        $('.container').find(`.${div}:empty:first`).html(a_tag + "<img src=" + pokemon.sprite + "></a>")
+      })
+    }
   })
 })
 
@@ -82,20 +106,57 @@ function pokeBattle() {
   var result = []
   for (var i = 0; i < your_roster.length; i++) {
     var ypoke = your_roster[i]
-    var score = 0
+    var enemyvsyourscore = 0
+    var yourvsenemyscore = 0
     for (var j = 0; j < enemy_roster.length; j++) {
       var epoke = enemy_roster[j]
+
       for (var k = 0; k < ypoke.weak_to.length; k++) {
         var yweak = ypoke.weak_to[k]
         for (var l = 0; l < epoke.types.length; l++) {
-          eweak = epoke.types[l].name
-          if (yweak == eweak) {
-            score--
+          etype = epoke.types[l].name
+          if (yweak == etype) {
+            enemyvsyourscore--
           }
         }
       }
-      if (score < 0) {
-        result.push(`${ypoke.name} is weak against ${epoke.name}!`)
+      for (var k = 0; k < ypoke.not_weak_to.length; k++) {
+        var ynotweak = ypoke.not_weak_to[k]
+        for (var l = 0; l < epoke.types.length; l++) {
+          etype = epoke.types[l].name
+          if (ynotweak == etype) {
+            enemyvsyourscore++
+          }
+        }
+      }
+      if (enemyvsyourscore < 0) {
+        result.push(`${epoke.name} is super effective against ${ypoke.name}!`)
+      } else if (enemyvsyourscore > 0) {
+        result.push(`${epoke.name} is not very effective against ${ypoke.name}!`)
+      }
+
+      for (var k = 0; k < ypoke.super_effective.length; k++) {
+        var ysupereff = ypoke.super_effective[k]
+        for (var l = 0; l < epoke.types.length; l++) {
+          etype = epoke.types[l].name
+          if (ysupereff == etype) {
+            yourvsenemyscore--
+          }
+        }
+      }
+      for (var k = 0; k < ypoke.not_very_effective.length; k++) {
+        var ynotveff = ypoke.not_very_effective[k]
+        for (var l = 0; l < epoke.types.length; l++) {
+          etype = epoke.types[l].name
+          if (ynotveff == etype) {
+            enemyvsyourscore++
+          }
+        }
+      }
+      if (yourvsenemyscore < 0) {
+        result.push(`${ypoke.name} is super effective against ${epoke.name}!`)
+      } else if (yourvsenemyscore > 0) {
+        result.push(`${ypoke.name} is not very effective against ${epoke.name}!`)
       }
     }
   }
@@ -103,25 +164,15 @@ function pokeBattle() {
 }
 
 function pokemonInfo(element, name) {
-  return $.get(`https://pokeapi.co/api/v2/pokemon/${name}`).
-    then((pokemon)=>{
-      let poke_types = pokemon.types.map(function(type_obj){
-        return type_obj.type.name
-      })
-      let poke_stats = pokemon.stats.map(function(stat_obj){
-        return `${stat_obj.stat.name}: ${stat_obj.base_stat}`
-      })
-      $('.info').html(`${poke_types}\n${poke_stats.join("\n")}`)
-      $('.info').css("display", "block")
+  let pokemon = allPokes.find(function(ele) {
+    return ele.name == name
   })
+  let poke_types = pokemon.types.map(function(type_obj){
+    return type_obj.name
+  })
+  let poke_stats = pokemon.stats.map(function(stat_obj){
+    return `${stat_obj.stat.name}: ${stat_obj.base_stat}`
+  })
+  $('.info').html(`${poke_types}\n${poke_stats.join("\n")}`)
+  $('.info').css("display", "block")
 }
-
-// function pokeCompare() {
-//
-// }
-
-//epoke.weak_to.push(0)
-// for (var k = 0; k < enemy_roster.length; k++) {
-//   var epoke = enemy_roster[k]
-//   for (var l = 0; l < epoke.types.length; l++) {
-//     var etype = epoke.types[l]
