059ac84155f74b6b20cc1b325e6a1f153eb0fabe
Devin Rieger <dev0089@gmail.com>
2017-03-29 01:12:01 -0400

    COMPARISONS YESSSSS

--- index.js
+++ index.js
@@ -1,35 +1,117 @@
+const numTypes = 18
+const allTypes = []
+Array.from({length:numTypes},(v,k)=>k+1).map(function(number) {
+  return $.get(`https://pokeapi.co/api/v2/type/${number}/`).
+  then((typeData) => {
+    allTypes.push(typeData)
+  })
+})
+
+allTypes.filter(function(type) {
+  return type.name == "ghost"
+})[0]
+
 class Pokemon {
-  constructor(number, name, types, sprite) {
+  constructor(number, name, types, sprite, team, weak_to, not_weak_to) {
     this.number = number,
     this.name = name,
     this.types = types,
     this.sprite = sprite
-    roster.push(this)
+    this.team = team
+    this.weak_to = weak_to
+    this.not_weak_to = not_weak_to
   }
-  static find(numOrName) {
+  static intialize(numOrName, team) {
     return $.get(`https://pokeapi.co/api/v2/pokemon/${numOrName}`).
     then((pokemon) => {
       var sprite = `https://assets-lmcrhbacy2s.stackpathdns.com/img/pokemon/animated/${pokemon.name}.gif`
-      return new Pokemon(pokemon.id, pokemon.name, pokemon.types, sprite)
+      var types = pokemon.types.map(function(ptype) {
+        return allTypes.filter(function(type) {
+          return type.name == ptype.type.name
+        })[0]
+      })
+      var pokemon = new Pokemon(pokemon.id, pokemon.name, types, sprite, team, [], [])
+      if (pokemon.team == 1) {
+        your_roster.push(pokemon)
+      } else {
+        enemy_roster.push(pokemon)
+      }
+      for (var j = 0; j < pokemon.types.length; j++) {
+        var type = pokemon.types[j]
+        for (var k = 0; k < type.damage_relations.double_damage_from.length; k++) {
+          pokemon.weak_to.push(type.damage_relations.double_damage_from[k].name)
+        }
+        for (var k = 0; k < type.damage_relations.half_damage_from.length; k++) {
+          pokemon.not_weak_to.push(type.damage_relations.half_damage_from[k].name)
+        }
+      }
+      return pokemon
     })
   }
 }
 
-var roster = []
+var your_roster = []
+var enemy_roster = []
 
 $(function() {
   $(".pokeSubmit").on("click", function() {
     event.preventDefault()
     var input = $("#numOrName").val()
-    Pokemon.find(input).
+    if (this.id == "1") {
+      var team = 1
+    } else {
+      var team = 2
+    }
+    Pokemon.intialize(input, team).
     then(function(pokemon){
-      $('.container').find('.pokemon:empty:first').html("<img src=" + pokemon.sprite + "><a href='#' onclick='removePokemon(this)'>X</a>")
+      if (pokemon.team == 1){
+        var div = $('.container').find('.pokemon:empty:first')
+      } else {
+        var div = $('.container').find('.matchup:empty:first')
+      }
+      div.html("<img src=" + pokemon.sprite + ">")
+      div.append("<a href='#' onclick='removePokemon(this)'>X</a>")
+      div.prepend(pokemon.name + '<br>')
     })
   })
 })
 
+
 function removePokemon(pokemon) {
   let i = pokemon
   $(i).parent().empty()
-
 }
+
+function pokeBattle() {
+  var result = []
+  for (var i = 0; i < your_roster.length; i++) {
+    var ypoke = your_roster[i]
+    var score = 0
+    for (var j = 0; j < enemy_roster.length; j++) {
+      var epoke = enemy_roster[j]
+      for (var k = 0; k < ypoke.weak_to.length; k++) {
+        var yweak = ypoke.weak_to[k]
+        for (var l = 0; l < epoke.types.length; l++) {
+          eweak = epoke.types[l].name
+          if (yweak == eweak) {
+            score--
+          }
+        }
+      }
+      if (score < 0) {
+        result.push(`${ypoke.name} is weak against ${epoke.name}!`)
+      }
+    }
+  }
+  console.log(result)
+}
+
+// function pokeCompare() {
+//
+// }
+
+//epoke.weak_to.push(0)
+// for (var k = 0; k < enemy_roster.length; k++) {
+//   var epoke = enemy_roster[k]
+//   for (var l = 0; l < epoke.types.length; l++) {
+//     var etype = epoke.types[l]
